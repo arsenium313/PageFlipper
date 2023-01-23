781c4da9539c61cb9714229b8380ecc5747c61ca
arsenium313 <arsenium313@gmail.com>
2023-01-23 17:18:50 +0300

    Initial Commit

---
+++ PageFlipper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageFlipper
+//
+//  Created by Арсений Кухарев on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
